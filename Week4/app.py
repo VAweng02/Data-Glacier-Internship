6304d6eb1d9caeb963fa3e4978ea1618ce6ea893
Vincent <vincent.weng@ipsos.com>
2023-03-25 14:00:36 -0400

    added background and fixed submission rendering issues

--- Week4/app.py
+++ Week4/app.py
@@ -15,12 +15,15 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
+    print('Received input:', int_features)
     final_features = [np.array(int_features)]
+    print('Final input:', final_features)
     prediction = model.predict(final_features)
 
-    output = prediction[0]
+    iris_names = ['Setosa', 'Versicolor', 'Virginica']
+    predicted_name = iris_names[int(prediction[0])]
 
-    return render_template('index.html', prediction_text='Predicted Iris Species: {}'.format(output))
+    return render_template('index.html', prediction_text='Predicted Iris Species: {}'.format(predicted_name))
 
 if __name__ == "__main__":
     app.run(debug=True)
